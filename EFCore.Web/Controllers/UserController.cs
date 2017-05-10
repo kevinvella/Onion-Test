052e0a48e3138cbf2b5c655aed5502ca3eb1880f
Kevin Vella <kevinv@holistic.com.mt>
2017-05-10 15:17:07 +0200

    Added User controller and DotNetCliToolReference

---
+++ EFCore.Web/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFCore.Services.Interfaces;
+using EFCore.Data.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        IUserService userService;
+
+        public UserController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = userService.GetAll();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]tb_User user)
+        {
+            if (user == null)
+                return BadRequest($"User object is null");
+
+            user.usr_Guid = Guid.NewGuid();
+            userService.Add(user);
+
+            return Ok();
+        }
+    }
+}
