72cd07d5b67318b60fc3b47ac9fafd6706827425
Kevin Vella <kevinv@holistic.com.mt>
2017-05-16 13:49:05 +0200

    Added unit test for the service layer

---
+++ EFCore.Services.UnitTest/MD5CryptographyServiceUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using EFCore.Services.Cryptography.Hashing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Services.UnitTest
+{
+    [TestClass]
+    public class MD5CryptographyServiceUnitTest
+    {
+        [TestMethod]
+        public void HashStringUnitTest()
+        {
+            string data = "testing123456789!";
+            string preHashedString = "F582DBED972F9BECC1FEC7041E491578";
+
+            MD5CryptographyService service = new MD5CryptographyService();
+
+            string hashedString = service.Hash(data);
+
+            Assert.AreEqual(preHashedString, hashedString);
+        }
+    }
+}
---
+++ EFCore.Services.UnitTest/UserServiceUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCore.Services.UnitTest
+{
+    [TestClass]
+    public class UserServiceUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
